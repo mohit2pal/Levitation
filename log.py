5521df66c3e3fab28e779e9a34f6597d08a1d338
SUBHA BAL PAL <91749681+mohit2pal@users.noreply.github.com>
2021-11-14 13:35:26 +0530

    log execution

---
+++ log.py
@@ -0,0 +1,6 @@
+def entry(a):
+    n=a
+    L=("\n",n,"\n")
+    f1=open('log.txt','a')
+    f1.writelines(L)
+    f1.close()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect
 from flask import request
 from forms import SignUpForm
+from log import * 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'levetation'
@@ -8,6 +9,14 @@ app.config['SECRET_KEY'] = 'levetation'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = SignUpForm()
+    if form.is_submitted():
+        result= request.form
+        name2 =request.form['name']
+        age2 =request.form['age']
+        mobile2 =request.form['mobile']
+        destination2 =request.form['destination']
+        seat2 =request.form['seat']
+        entry(name2)
     return render_template('index.html', form=form)
     
 if __name__ == '__main__':
