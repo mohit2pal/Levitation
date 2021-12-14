82481b777ce5cd9e2387657dab6f24c2a2cd1793
Abhishek Yadav <abhishekaug2002@gmail.com>
2021-12-14 20:28:55 +0530

    trial

--- alllot.py
+++ alllot.py
@@ -2,10 +2,10 @@ def allot():
     import datetime
     now=datetime.datetime.now()
     time=now.strftime("%H:%M")
-    if(time>'20:40' and time<'20:44'):
+    if(time>'00:20' and time<'00:33'):
         a='A1'
         return(a)
-    elif(time>'20:49' and time<'20:57'):
+    elif(time>'00:50' and time<'00:57'):
         a='A2'
         return(a)
     else:
--- seat.py
+++ seat.py
@@ -1,7 +1,7 @@
 def seat(b):
     if(b=='A1'):
        s1=['1','2','3','4','5']
-    #for i in range (0,2):
+   # for i in range (0,2):
        print("CHOOSE YOUR SEAT FROM THESE: ", s1,"\n")
        e=input("Enter:   ")
        s1.remove(e)
@@ -12,6 +12,8 @@ def seat(b):
       print("CHOOSE YOUR SEAT FROM THESE: ", s2,"\n")
       e=input("Enter:   ")
       s2.remove(e)
-      print("thanku")
+      print(s2)
+     
+    #print("thanku")
     
     
