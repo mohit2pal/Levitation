4bf1c398c800579a058323bbfc1e507db802018d
Abhishek Yadav <abhishekaug2002@gmail.com>
2021-12-18 18:08:46 +0530

    Update seat.py

--- seat.py
+++ seat.py
@@ -1,4 +1,4 @@
-s=['1','2','3','4','5']
+'''s=['1','2','3','4','5']
 
 def seat(b):
    global s
@@ -15,6 +15,29 @@ def seat(b):
       print("CHOOSE YOUR SEAT FROM THESE: ", s2,"\n")
       e=input("Enter:   ")
       s2.remove(e)
-      print("thanku")
+      print("thanku")'''
+
+s=['1','2','3','4','5']
+
+def seat(b):
+   global s
+   a=64
+   for i in range(1,14):
+      a=a+1
+      c=1
+      for j in range(1,7):
+           platform_ascii=chr(a)
+           platform= str(platform_ascii)
+           bay= str(c)
+           c=c+1
+           str2= platform + bay
+           print(str2)
+           if(b== str2):
+             print("CHOOSE YOUR SEAT FROM THESE: ", s,"\n")
+             e=input("Enter:   ")
+             s.remove(e)
+             print(s)
+   
+seat("M1")   
     
     
