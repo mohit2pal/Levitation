85cd9b023beb6471add4428c298d47bc3a836a32
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2021-12-19 13:48:01 +0530

    length added

--- forms.py
+++ forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms import validators
+from wtforms.fields.numeric import IntegerField
+from wtforms.validators import Length, DataRequired
+
+
+
 
 class SignUpForm(FlaskForm):
-    name = StringField('Customer Name*')
-    age = StringField('Enter your age*')
-    mobile = StringField('Mobile Number*')
-    destination = StringField('Destination*')
+    name = StringField('Customer Name*', validators=[DataRequired(), Length(max = 20)])
+    age = StringField('Enter your age*', validators=[DataRequired(), Length(max = 3)])
+    mobile = StringField('Mobile Number*',validators=[DataRequired(), Length(max = 10)])
+    destination = StringField('Destination*', validators=[DataRequired(), Length(max = 20)] )
     submit = SubmitField('Next')
