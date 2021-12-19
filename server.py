682e7e585aaaf9b264f65dac405a278f4d29add3
SUBHA BAL PAL <91749681+mohit2pal@users.noreply.github.com>
2021-12-19 14:42:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -16,16 +16,18 @@ def home():
 def index():
     form = SignUpForm()
     if form.is_submitted():
-        result= request.form
-        name2 =request.form['name']
-        age2 =request.form['age']
-        mobile2 =request.form['mobile']
-        destination2 =request.form['destination']
-        entry(name2,age2,mobile2,destination2)
-        allot2 = allot()
-        print(allot2)
-        seat(allot2)
-        return render_template('seat.html', nameh=name2, ageh=age2, destinationh=destination2)
+      if form.is_submitted():
+          result= request.form
+          name2 =request.form['name']
+          age2 =request.form['age']
+          mobile2 =request.form['mobile']
+          destination2 =request.form['destination']
+          entry(name2,age2,mobile2,destination2)
+          allot2 = allot()
+          print(allot2)
+        #   seat(allot2)
+          return render_template('output.html', nameh=name2, ageh=age2, destinationh=destination2, alloth = allot2)
+      return render_template('seat.html')
     return render_template('index.html', form=form)
     
 @app.route('/pod_bay_ticket')
