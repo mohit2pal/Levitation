7b182e7910177606f397e7d9c6a425fdd84945fa
SUBHA BAL PAL <91749681+mohit2pal@users.noreply.github.com>
2021-12-19 15:24:22 +0530

    fixes

--- alllot.py
+++ alllot.py
@@ -2,7 +2,7 @@ def allot():
     import datetime
     now=datetime.datetime.now()
     time=now.strftime("%H:%M")
-    if(time>'20:40' and time<'20:44'):
+    if(time>'15:20' and time<'15:44'):
         a='A1'
         return(a)
     elif(time>'20:49' and time<'20:57'):
