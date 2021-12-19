77e1f32ecad118dabf79ce8a3d2985cb523889f0
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2021-12-19 10:59:13 +0530

    required added

--- forms.py
+++ forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 class SignUpForm(FlaskForm):
-    name = StringField('Customer Name')
-    age = StringField('Enter your age')
-    mobile = StringField('Mobile Number')
-    destination = StringField('Destination')
+    name = StringField('Customer Name*')
+    age = StringField('Enter your age*')
+    mobile = StringField('Mobile Number*')
+    destination = StringField('Destination*')
     submit = SubmitField('Next')
