ede2aa8a730609627b932f42818667ebc17dedc3
Abhishek Yadav <abhishekaug2002@gmail.com>
2021-12-19 14:03:14 +0530

    seat updated

--- seat.py
+++ seat.py
@@ -1,25 +1,6 @@
-'''s=['1','2','3','4','5']
+'''s=['1','2','3','4','5']'''
 
-def seat(b):
-   global s
-   if(b=='A6'):
-       #s1=['1','2','3','4','5']
-    #for i in range (0,2):
-       print("CHOOSE YOUR SEAT FROM THESE: ", s,"\n")
-       e=input("Enter:   ")
-       s2 = s.remove(e)
-       print(s2)
-   if(b=='A1'):
-      s2=['1','2','3','4','5']
-    #for i in range (0,2):
-      print("CHOOSE YOUR SEAT FROM THESE: ", s2,"\n")
-      e=input("Enter:   ")
-      s2.remove(e)
-      print("thanku")'''
-
-s=['1','2','3','4','5']
-
-def seat(b):
+'''def seat(b):
    global s
    a=64
    for i in range(1,14):
@@ -38,6 +19,46 @@ def seat(b):
              s.remove(e)
              print(s)
    
-seat("M1")   
+seat("M1")   '''
     
-    
+s=['1','2','3','4','5'] 
+store = ""
+
+def seat(b):
+ global s
+ global store
+ a=64
+
+ if(b==store):
+     '''for i in range(1,14):
+      a=a+1
+      c=1
+      for j in range(1,7):
+           platform_ascii=chr(a)
+           platform= str(platform_ascii)          
+           bay= str(c)
+           c=c+1
+           str2= platform + bay
+           print(str2)
+           if(b== str2):'''
+            
+     print("CHOOSE YOUR SEAT FROM THESE: ", s,"\n")
+     e=input("Enter:   ")
+     s.remove(e)
+     print(s)
+
+ else:
+      s=['1','2','3','4','5'] 
+      print("CHOOSE YOUR SEAT FROM THESE: ", s,"\n")
+      e=input("Enter:   ")
+      s.remove(e)
+      print(s)
+
+ store=b
+
+
+seat("A1")
+print(store)
+seat("A1")
+seat("A3")
+seat("A3")   
