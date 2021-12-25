4d7ee42faa7c96511fa61b33b6848bd15470ef42
SUBHA BAL PAL <91749681+mohit2pal@users.noreply.github.com>
2021-12-25 16:40:42 +0530

    Update booking.js

--- static/js/booking.js
+++ static/js/booking.js
@@ -1,19 +1,55 @@
 const container = document.querySelector(".container")
 const seats = document.querySelectorAll(".row .seat")
 const button = document.querySelector(".button")
+ 
+// for storing value in local storage
+function store() {
+    const bookedSeats = document.querySelectorAll(".row .seat.booked, .row .seat.sold")
+    const seatsIndex = [...bookedSeats].map((seat) => [...seats].indexOf(seat))
 
+    localStorage.setItem("bookedSeats", JSON.stringify(seatsIndex))
+}
 // used to listen seat clicking and toggling color
 container.addEventListener("click", (e) => {
     if(e.target.classList.contains("seat") && !e.target.classList.contains("sold")) {
         e.target.classList.toggle("booked")
     }
-    const bookedSeats = document.querySelectorAll(".row .seat.booked")
-    const seatsIndex = [...bookedSeats].map((seat) => [...seats].indexOf(seat))
+    // const bookedSeats = document.querySelectorAll(".row .seat.booked")
+    // const seatsIndex = [...bookedSeats].map((seat) => [...seats].indexOf(seat))
 
-    // console.log(bookedSeats)
+    // localStorage.setItem("bookedSeats", JSON.stringify(seatsIndex))
+    // const selectedSeatsCount = selectedSeats.length;
+
+    console.log(bookedSeats)
     console.log(seatsIndex)
 })
 
 button.addEventListener("click", () => {
-    console.log("6")
+
+    store()
+    const selectedSeats = JSON.parse(localStorage.getItem("bookedSeats"))
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add("sold")
+            }
+        })
+    }
+    console.log(e)
+    console.log("9")
 })
+
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem("bookedSeats"))
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add("sold")
+            }
+        })
+}
+}
+
+populateUI()
