79c70062b0c8d79dd70a4db51720a6a6199e09c8
Abhishek Yadav <abhishekaug2002@gmail.com>
2022-01-10 17:52:54 +0530

    worker login alert box fixed
    alert box now comes at right position with blinking facility

--- static/js/login.js
+++ static/js/login.js
@@ -12,6 +12,17 @@ function submitted() {
         // xhr.send(check)
         window.open("/worker","_self")
     } else {
-        alert("Please enter proper username and password")
+        functionAlert();
     }
+}
+
+function functionAlert(msg, myYes) {
+    var confirmBox = $("#confirm");
+    confirmBox.find(".message").text(msg);
+    confirmBox.find(".yes").unbind().click(function() {
+       confirmBox.hide();
+    });
+    confirmBox.find(".yes").click(myYes);
+    confirmBox.show();
+
 }
