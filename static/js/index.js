bc670ab1513f8a3358cd18318b6e7d26d0958061
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2022-01-26 15:38:03 +0530

    index fixed

--- static/js/index.js
+++ static/js/index.js
@@ -10,7 +10,7 @@ function status_update() {
         console.log(block['status'])
 
         if(block['status'] == 1){
-            document.getElementById('app').innerHTML = "Allowed"
+            document.getElementById('app').innerHTML = "Booking is Allowed"
         }
         else if(block['status'] == 0) {
             // document.getElementById('app').innerHTML = "Blocked"
