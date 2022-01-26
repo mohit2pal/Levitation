47c75ec0a225beb216dc300d25fad6172636f6db
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2022-01-26 11:01:15 +0530

    index and waccess  done

--- static/js/index.js
+++ static/js/index.js
@@ -8,10 +8,10 @@ function status_update() {
         console.log(block['status'])
 
         if(block['status'] == 1){
-            document.getElementById('app').innerHTML = "Allowed"
+            document.getElementById('app').innerHTML = "Seat Booking is Allowed"
         }
         else if(block['status'] == 0) {
-            document.getElementById('app').innerHTML = "Blocked"
+            document.getElementById('app').innerHTML = "Seat Booking is Blocked"
         }
     }
 
