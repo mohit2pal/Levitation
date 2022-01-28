ee3179f2d6f34696e7c61fed08379fe5f621173e
SUBHA BAL PAL <91749681+mohit2pal@users.noreply.github.com>
2022-01-28 22:53:11 +0530

    Update pod_selector.js

--- static/js/pod_selector.js
+++ static/js/pod_selector.js
@@ -1,21 +1,62 @@
-const b1 = document.getElementById("button1").addEventListener("click", caller)
-const b2 = document.getElementById("button2").addEventListener("click", caller)
-const b3 = document.getElementById("button3").addEventListener("click", caller)
-const b4 = document.getElementById("button4").addEventListener("click", caller)
-const b5 = document.getElementById("button5").addEventListener("click", caller)
-const b6 = document.getElementById("button6").addEventListener("click", caller)
-const b7 = document.getElementById("button7").addEventListener("click", caller)
-const b8 = document.getElementById("button8").addEventListener("click", caller)
-const b9 = document.getElementById("button9").addEventListener("click", caller)
-const b10 = document.getElementById("button10").addEventListener("click", caller)
-const b11 = document.getElementById("button11").addEventListener("click", caller)
-const b12 = document.getElementById("button12").addEventListener("click", caller)
-const b13 = document.getElementById("button13").addEventListener("click", caller)
+const b1 = document.getElementById("button1").addEventListener("click", deflector1)
+const b2 = document.getElementById("button2").addEventListener("click", deflector2)
+const b3 = document.getElementById("button3").addEventListener("click", deflector3)
+const b4 = document.getElementById("button4").addEventListener("click", deflector4)
+const b5 = document.getElementById("button5").addEventListener("click", deflector5)
+const b6 = document.getElementById("button6").addEventListener("click", deflector6)
+const b7 = document.getElementById("button7").addEventListener("click", deflector7)
+const b8 = document.getElementById("button8").addEventListener("click", deflector8)
+const b9 = document.getElementById("button9").addEventListener("click", deflector9)
+const b10 = document.getElementById("button10").addEventListener("click", deflector10)
+const b11 = document.getElementById("button11").addEventListener("click", deflector11)
+const b12 = document.getElementById("button12").addEventListener("click", deflector12)
+const b13 = document.getElementById("button13").addEventListener("click", deflector13)
+
+function deflector1() {
+  caller(1)
+}
+function deflector2() {
+  caller(2)
+}
+function deflector3() {
+  caller(3)
+}
+function deflector4() {
+  caller(4)
+}
+function deflector5() {
+  caller(5)
+}
+function deflector6() {
+  caller(6)
+}
+function deflector7() {
+  caller(7)
+}
+function deflector8() {
+  caller(8)
+}
+function deflector9() {
+  caller(9)
+}
+function deflector10() {
+  caller(10)
+}
+function deflector11() {
+  caller(11)
+}
+function deflector12() {
+  caller(12)
+}
+function deflector13() {
+  caller(13)
+}
 
 
+var data = ''
 
 function caller(d){
-
+  console.log(d)
   var chr = new XMLHttpRequest()
   chr.open("GET", "./static/js/damage.json", true)
 
@@ -38,28 +79,27 @@ chr.send()
 
 function send_group(t) {
 
-  if(t==1){const data = 'B,C,D,E,F,G,H,I,J,K,L,M'}
-  else if(t==2){const data = 'A,C,D,E,F,G,H,I,J,K,L,M'}
-  else if(t==3){const data = 'A,B,D,E,F,G,H,I,J,K,L,M'}
-  else if(t==4){const data = 'A,B,C,E,F,G,H,I,J,K,L,M'}
-  else if(t==5){const data = 'A,B,C,D,F,G,H,I,J,K,L,M'}
-  else if(t==6){const data = 'A,B,C,D,E,G,H,I,J,K,L,M'}
-  else if(t==7){const data = 'A,B,C,D,E,F,H,I,J,K,L,M'}
-  else if(t==8){const data = 'A,B,C,D,E,F,G,I,J,K,L,M'}
-  else if(t==9){const data = 'A,B,C,D,E,F,G,H,J,K,L,M'}
-  else if(t==10){const data = 'A,B,C,D,E,F,G,H,I,K,L,M'}
-  else if(t==11){const data = 'A,B,C,D,E,F,G,H,I,J,L,M'}
-  else if(t==12){const data = 'A,B,C,D,E,F,G,H,I,J,K,M'}
-  else if(t==13){const data = 'A,B,C,D,E,F,G,H,I,J,K,L'}
+  if(t==1){data = 'B,C,D,E,F,G,H,I,J,K,L,M'}
+  else if(t==2){data = 'A,C,D,E,F,G,H,I,J,K,L,M'}
+  else if(t==3){data = 'A,B,D,E,F,G,H,I,J,K,L,M'}
+  else if(t==4){data = 'A,B,C,E,F,G,H,I,J,K,L,M'}
+  else if(t==5){data = 'A,B,C,D,F,G,H,I,J,K,L,M'}
+  else if(t==6){data = 'A,B,C,D,E,G,H,I,J,K,L,M'}
+  else if(t==7){data = 'A,B,C,D,E,F,H,I,J,K,L,M'}
+  else if(t==8){data = 'A,B,C,D,E,F,G,I,J,K,L,M'}
+  else if(t==9){data = 'A,B,C,D,E,F,G,H,J,K,L,M'}
+  else if(t==10){data = 'A,B,C,D,E,F,G,H,I,K,L,M'}
+  else if(t==11){data = 'A,B,C,D,E,F,G,H,I,J,L,M'}
+  else if(t==12){data = 'A,B,C,D,E,F,G,H,I,J,K,M'}
+  else if(t==13){data = 'A,B,C,D,E,F,G,H,I,J,K,L'}
   
-    
+  const datat = data
 
-    var xhr = new XMLHttpRequest()
-    xhr.open('POST', '/pod_selector', true)
-    xhr.setRequestHeader('Content-Type', 'text/plain')
+  var xhr = new XMLHttpRequest()
+  xhr.open('POST', '/pod_selector', true)
+  xhr.setRequestHeader('Content-Type', 'text/plain')
     
-    xhr.send(data)
-    caller()
+  xhr.send(datat)
 }
 
 function sound() {
@@ -102,6 +142,4 @@ function()
 //     // dicto = {"clicks": clicks }
 //     // json_object = json.dumps(dicto, indent = 1)
 //     // with open("./static/js/block.json", "w") as outfile:
-//     //     outfile.write(json_object)
-
-// })
+//     //     outfile.write(json_object, () => { caller(1)}
